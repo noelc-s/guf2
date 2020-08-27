fa6b5584d176a5d95ff1d01363d5d3460e1785ff
Kellan <kmoorse@caltech.edu>
2020-08-27 11:17:04 -0700

    convert 1D wing hinges to spherical joints

--- fly_model/flysim.py
+++ fly_model/flysim.py
@@ -10,11 +10,40 @@ planeId = p.loadURDF("plane.urdf")
 
 # Load fly model
 flyStartPos = [0,0,3]
-flyStartOrientation = p.getQuaternionFromEuler([0,0,0])
+flyStartOrientation = p.getQuaternionFromEuler([.75,0,0])
 flyId = p.loadURDF("fly.urdf", flyStartPos, flyStartOrientation)
+
+nj = p.getNumJoints(flyId)
+for i in range(nj):
+    ji = p.getJointInfo(flyId, i)
+    print(ji[:2])
+
+sign = 1
+
+p.resetJointState(flyId, 4, -1)
+p.resetJointState(flyId, 7, 1)
 
 # Run simulation
 for i in range (10000):
+
+    if i%100==0:
+        p.setJointMotorControl2(
+            flyId,4,
+            controlMode=p.VELOCITY_CONTROL,
+            targetVelocity=5*sign,
+            force=500
+            )
+        p.setJointMotorControl2(
+            flyId,7,
+            controlMode=p.VELOCITY_CONTROL,
+            targetVelocity=-5*sign,
+            force=500
+            )
+        sign = -sign
+
+    js = p.getJointState(flyId,2)
+    # print(js)
+
     p.stepSimulation()
     time.sleep(1./240.)
 
